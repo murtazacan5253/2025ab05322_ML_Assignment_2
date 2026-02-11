560493e9ed366b76cd0984b32907d20965a90df7
Murtaza Canteenwala <murtaza.canteenwala@ericsson.com>
2026-02-12 01:30:20 +0530

    models

--- dataPreProcFeatureEng.py
+++ dataPreProcFeatureEng.py
@@ -5,57 +5,60 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
-# Add column names as not present in the UCI repo data
-columns = [
-    "age","workclass","fnlwgt","education","education_num",
-    "marital_status","occupation","relationship","race","sex",
-    "capital_gain","capital_loss","hours_per_week",
-    "native_country","income"
-]
+def dataPreProcFeatureEng():
 
-df = pd.read_csv(
-    "adult.data",
-    header=None,
-    names=columns,
-    sep=",",
-    skipinitialspace=True
-)
+    # Add column names as not present in the UCI repo data
+    columns = [
+        "age","workclass","fnlwgt","education","education_num",
+        "marital_status","occupation","relationship","race","sex",
+        "capital_gain","capital_loss","hours_per_week",
+        "native_country","income"
+    ]
 
-# Handle missing values, if any
-print((df.isin(["?", " ?"])).sum())
+    df = pd.read_csv(
+        "adult.data",
+        header=None,
+        names=columns,
+        sep=",",
+        skipinitialspace=True
+    )
 
-# Convert "?" → NaN
-df.replace((["?", " ?"]), np.nan, inplace=True) 
+    # Handle missing values, if any
+    print((df.isin(["?", " ?"])).sum())
 
-# Drop rows with missing values
-df.dropna(inplace=True)
+    # Convert "?" → NaN
+    df.replace((["?", " ?"]), np.nan, inplace=True) 
 
-# Separate Features & Target
-X = df.drop("income", axis=1)
-y = df["income"]
+    # Drop rows with missing values
+    df.dropna(inplace=True)
 
-# Encode Target Variable
-encode_target = LabelEncoder()
-y = encode_target.fit_transform(y)
+    # Separate Features & Target
+    X = df.drop("income", axis=1)
+    y = df["income"]
 
-# One-Hot Encode Categorical Features
-X = pd.get_dummies(X, drop_first=True)
+    # Encode Target Variable
+    encode_target = LabelEncoder()
+    y = encode_target.fit_transform(y)
 
-# Train/Test Split
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y,
-    test_size=0.2,
-    random_state=42,
-    stratify=y
-)
+    # One-Hot Encode Categorical Features
+    X = pd.get_dummies(X, drop_first=True)
 
-# Feature Scaling
-scaler = StandardScaler()
+    # Train/Test Split
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y,
+        test_size=0.2,
+        random_state=42,
+        stratify=y
+    )
 
-X_train_scaled = scaler.fit_transform(X_train)
-X_test_scaled = scaler.transform(X_test)
+    # Feature Scaling
+    scaler = StandardScaler()
 
-# Verification
-print("Train shape:", X_train_scaled.shape)
-print("Test shape:", X_test_scaled.shape)
+    X_train_scaled = scaler.fit_transform(X_train)
+    X_test_scaled = scaler.transform(X_test)
 
+    # Verification
+    print("Train shape:", X_train_scaled.shape)
+    print("Test shape:", X_test_scaled.shape)
+
+    return X_train, X_test, X_train_scaled, X_test_scaled, y_train, y_test
