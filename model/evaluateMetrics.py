d951c96d7414b329e98d8a71bc138333bbd63d85
Murtaza Canteenwala <murtaza.canteenwala@ericsson.com>
2026-02-13 13:18:51 +0530

    metric

--- model/evaluateMetrics.py
+++ model/evaluateMetrics.py
@@ -1,71 +1,20 @@
 # Import Dependencies
 from sklearn.metrics import *
-from modelPrediction import *
+from modelPrediction import modelPredict
 
+def getMetrics (model,X_test,y_test):
+    # Model Prediction
+    pred, prob= modelPredict(model, X_test)
 
-# Logicstic Regression Metrics Evaluation
-pred, prob, y_test = predLogisticRegression()
-print("Metrics for Logistic Regression")
-print("===========================================")
-print("Accuracy:", accuracy_score(y_test,pred))
-print("AUC:", roc_auc_score(y_test,prob))
-print("Precision:", precision_score(y_test,pred))
-print("Recall:", recall_score(y_test,pred))
-print("F1:", f1_score(y_test,pred))
-print("MCC:", matthews_corrcoef(y_test,pred))
+    # Model Metric Score
+    print("Metrics")
+    print("===========================================")
+    print("Accuracy:", accuracy_score(y_test,pred))
+    print("AUC:", roc_auc_score(y_test,prob))
+    print("Precision:", precision_score(y_test,pred))
+    print("Recall:", recall_score(y_test,pred))
+    print("F1:", f1_score(y_test,pred))
+    print("MCC:", matthews_corrcoef(y_test,pred))
 
-# Decision Tree Metrics Evaluation
-y_test, pred, y_test = predDecisionTree()
-print("Metrics for Decision Tree Classifier")
-print("===========================================")
-print("Accuracy:", accuracy_score(y_test,pred))
-print("AUC:", roc_auc_score(y_test,prob))
-print("Precision:", precision_score(y_test,pred))
-print("Recall:", recall_score(y_test,pred))
-print("F1:", f1_score(y_test,pred))
-print("MCC:", matthews_corrcoef(y_test,pred))
-
-# KNN Classifier Metrics Evaluation
-y_test, pred, y_test = predKnnClassifier()
-print("Metrics for KNN Classifier")
-print("===========================================")
-print("Accuracy:", accuracy_score(y_test,pred))
-print("AUC:", roc_auc_score(y_test,prob))
-print("Precision:", precision_score(y_test,pred))
-print("Recall:", recall_score(y_test,pred))
-print("F1:", f1_score(y_test,pred))
-print("MCC:", matthews_corrcoef(y_test,pred))
-
-# Naive Bayes Classifier Metrics Evaluation
-y_test, pred, y_test = predNaiveBayesClassifier()
-print("Metrics for Naive Bayes Classifier")
-print("===========================================")
-print("Accuracy:", accuracy_score(y_test,pred))
-print("AUC:", roc_auc_score(y_test,prob))
-print("Precision:", precision_score(y_test,pred))
-print("Recall:", recall_score(y_test,pred))
-print("F1:", f1_score(y_test,pred))
-print("MCC:", matthews_corrcoef(y_test,pred))
-
-# Random Forest Metrics Evaluation
-y_test, pred, y_test = predRandomForest()
-print("Metrics for random Forest")
-print("===========================================")
-print("Accuracy:", accuracy_score(y_test,pred))
-print("AUC:", roc_auc_score(y_test,prob))
-print("Precision:", precision_score(y_test,pred))
-print("Recall:", recall_score(y_test,pred))
-print("F1:", f1_score(y_test,pred))
-print("MCC:", matthews_corrcoef(y_test,pred))
-
-# XG Boost Metrics Evaluation
-y_test, pred, y_test = predXGBoost()
-print("Metrics for XG Boost")
-print("===========================================")
-print("Accuracy:", accuracy_score(y_test,pred))
-print("AUC:", roc_auc_score(y_test,prob))
-print("Precision:", precision_score(y_test,pred))
-print("Recall:", recall_score(y_test,pred))
-print("F1:", f1_score(y_test,pred))
-print("MCC:", matthews_corrcoef(y_test,pred))
+    
 
--- model/modelPrediction.py
+++ model/modelPrediction.py
@@ -1,39 +1,7 @@
 # Import Dependencies
-import joblib
 
 
-def  predLogisticRegression(X_test, y_test):
-    model = joblib.load("model/logistic.pkl")
+def  modelPredict(model, X_test):
     y_pred = model.predict(X_test)
     y_prob = model.predict_proba(X_test)[:, 1]
-    return (y_pred, y_prob, y_test)
-
-def  predKnnClassifier(X_test, y_test):
-    model = joblib.load("model/knn.pkl")
-    y_pred = model.predict(X_test)
-    y_prob = model.predict_proba(X_test)[:, 1]
-    return (y_pred, y_prob, y_test)
-
-def  predDecisionTree(X_test, y_test):
-    model = joblib.load("model/tree.pkl")
-    y_pred = model.predict(X_test)
-    y_prob = model.predict_proba(X_test)[:, 1]
-    return (y_pred, y_prob, y_test)
-
-def  predNaiveBayesClassifier(X_test, y_test):
-    model = joblib.load("model/nb.pkl")
-    y_pred = model.predict(X_test)
-    y_prob = model.predict_proba(X_test)[:, 1]
-    return (y_pred, y_prob, y_test)
-
-def  predRandomForest(X_test, y_test):
-    model = joblib.load("model/rf.pkl")
-    y_pred = model.predict(X_test)
-    y_prob = model.predict_proba(X_test)[:, 1]
-    return (y_pred, y_prob, y_test)
-
-def  predXGBoost(X_test, y_test):
-    model = joblib.load("model/xgb.pkl")
-    y_pred = model.predict(X_test)
-    y_prob = model.predict_proba(X_test)[:, 1]
-    return (y_pred, y_prob, y_test)
+    return (y_pred, y_prob)
