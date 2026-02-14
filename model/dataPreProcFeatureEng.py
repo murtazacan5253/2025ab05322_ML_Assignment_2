7cf7ddd524a83170e30f576b6ff01d909a30f733
Murtaza Canteenwala <murtaza.canteenwala@ericsson.com>
2026-02-14 14:05:47 +0530

    trainedModels

--- model/dataPreProcFeatureEng.py
+++ model/dataPreProcFeatureEng.py
@@ -6,57 +6,59 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
-#def dataPreProcFeatureEng_TrainData():
+def dataPreProcFeatureEng_TrainData():
 
-adultIncome_data = fetch_openml('adult', version=2, as_frame=True)
-df=adultIncome_data.frame
+    adultIncome_data = fetch_openml('adult', version=2, as_frame=True)
+    df=adultIncome_data.frame
 
-# Information about dataset
-df.info()
-
-
-# Remove irrelavant columns
-df = df.drop(columns=['fnlwgt'])
-df = df.drop(columns=['education-num'])
-
-# Handle missing values, if any
-# Convert "?" → NaN
-df.replace((["?", " ?"]), np.nan, inplace=True)
-
-# Drop rows with missing values
-df.dropna(inplace=True)
-
-# Separate Features & Target
-target_col = 'class' # Standard target name in OpenML version
-X = df.drop(columns=[target_col])
-y = df[target_col]
-
-#Share before data pre-processing
-print("Train shape:", X.shape)
+    # Information about dataset
+    df.info()
 
 
-# Encode Target Variable
-encode_target_train = LabelEncoder()
-y = encode_target_train.fit_transform(y)
+    # Remove irrelavant columns
+    df = df.drop(columns=['fnlwgt'])
+    df = df.drop(columns=['education-num'])
 
-# One-Hot Encode Categorical Features
-X = pd.get_dummies(X, drop_first=True)
+    # Handle missing values, if any
+    # Convert "?" → NaN
+    df.replace((["?", " ?"]), np.nan, inplace=True)
 
-# Train/Test Split
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y,
-    test_size=0.2,
-    random_state=42,
-    stratify=y
-)
+    # Drop rows with missing values
+    df.dropna(inplace=True)
 
-# Feature Scaling
-scaler = StandardScaler()
+    # Separate Features & Target
+    target_col = 'class' # Standard target name in OpenML version
+    X = df.drop(columns=[target_col])
+    y = df[target_col]
 
-X_train_scaled = scaler.fit_transform(X_train)
-X_test_scaled = scaler.transform(X_test)
+    #Share before data pre-processing
+    print("Train shape:", X.shape)
 
 
-# Verification
-print("Train shape:", X_train_scaled.shape)
-print("Test shape:", X_test_scaled.shape)
+    # Encode Target Variable
+    encode_target_train = LabelEncoder()
+    y = encode_target_train.fit_transform(y)
+
+    # One-Hot Encode Categorical Features
+    X = pd.get_dummies(X, drop_first=True)
+
+    # Train/Test Split
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y,
+        test_size=0.2,
+        random_state=42,
+        stratify=y
+    )
+
+    # Feature Scaling
+    scaler = StandardScaler()
+
+    X_train_scaled = scaler.fit_transform(X_train)
+    X_test_scaled = scaler.transform(X_test)
+
+
+    # Verification
+    print("Train shape:", X_train_scaled.shape)
+    print("Test shape:", X_test_scaled.shape)
+
+    return(X_train, X_test, X_train_scaled, X_test_scaled, y_train, y_test)
